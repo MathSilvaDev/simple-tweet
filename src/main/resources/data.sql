7c7ebdbc1304104f2c3aca9550f31468cf7ad7e3
Matheus Silva <mathsilva.code@gmail.com>
2026-01-05 14:24:14 -0300

    properties configured for H2 database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,2 @@
-CREATE TABLE IF NOT EXISTS tb_roles (
-    role_id INT PRIMARY KEY,
-    name VARCHAR(50) UNIQUE
-);
-
-INSERT IGNORE INTO tb_roles (role_id, name) VALUES (1, 'ADMIN');
-INSERT IGNORE INTO tb_roles (role_id, name) VALUES (2, 'BASIC');
+MERGE INTO tb_roles (role_id, name) KEY(role_id) VALUES (1, 'ADMIN');
+MERGE INTO tb_roles (role_id, name) KEY(role_id) VALUES (2, 'BASIC');
